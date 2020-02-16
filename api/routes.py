9d9c9ae6db586c04bc429afc1749aa7172ab2b29
Sosolidkk <pedrochaveslimas3@gmail.com>
2020-02-16 00:55:55 -0300

    Refactor on folder structure, added new route, and Readme edit

---
+++ api/routes.py
@@ -0,0 +1,27 @@
+from api import app, scraping, base_models
+
+
+@app.get("/")
+async def index():
+    return {"status": 200}
+
+
+@app.post("/info")
+async def return_info(user: base_models.User):
+    session, response = scraping.login(user.username, user.password)
+    info = scraping.grab_user_data(session, response)
+
+    return info
+
+
+@app.post("/grades")
+async def return_grades(user: base_models.User):
+    session, response = scraping.login(user.username, user.password)
+    info = scraping.see_all_grades(session, response)
+
+    return info
+
+
+@app.post("/subjects")
+async def return_subjects(user: base_models.User):
+    pass
