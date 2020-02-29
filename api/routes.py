7bd1c3946424b4eb9ae90df6a207471df6c1add4
Sosolidkk <pedrochaveslimas3@gmail.com>
2020-02-29 12:19:09 -0300

    Removed async options on func since requests don't support it

--- api/routes.py
+++ api/routes.py
@@ -3,12 +3,12 @@ from starlette.responses import RedirectResponse
 
 
 @app.get("/")
-async def index():
+def index():
     return RedirectResponse(url="/docs")
 
 
 @app.post("/info")
-async def return_info(user: base_models.User):
+def return_info(user: base_models.User):
     session, response = scraping.login(user.username, user.password)
     info = scraping.grab_user_data(session, response)
 
@@ -16,7 +16,7 @@ async def return_info(user: base_models.User):
 
 
 @app.post("/ver-notas")
-async def return_grades(user: base_models.User):
+def return_grades(user: base_models.User):
     session, response = scraping.login(user.username, user.password)
     info = scraping.see_all_grades(session, response)
 
@@ -24,7 +24,7 @@ async def return_grades(user: base_models.User):
 
 
 @app.post("/disciplinas")
-async def return_subjects(user: base_models.User):
+def return_subjects(user: base_models.User):
     _, response = scraping.login(user.username, user.password)
     info = scraping.see_all_subjects(response)
 
@@ -32,7 +32,7 @@ async def return_subjects(user: base_models.User):
 
 
 @app.post("/historico")
-async def return_history(user: base_models.User):
+def return_history(user: base_models.User):
     session, response = scraping.login(user.username, user.password)
     payload = scraping.grab_user_history(session, response)
 
@@ -40,7 +40,7 @@ async def return_history(user: base_models.User):
 
 
 @app.post("/declaracao")
-async def return_registration_statement(user: base_models.User):
+def return_registration_statement(user: base_models.User):
     session, response = scraping.login(user.username, user.password)
     payload = scraping.grab_user_registration_statement(session, response)
 
